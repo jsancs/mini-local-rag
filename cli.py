f5710bde36c525f11561b5e5fffd6ef50fb2c783
jsancs <javivisan19@gmail.com>
2024-10-10 19:09:27 +0200

    help instructions and exit control

--- cli.py
+++ cli.py
@@ -4,6 +4,35 @@ from prompt_toolkit.shortcuts import prompt
 from chat import chat_streaming, handle_model, add_msg_to_memory
 
 
+def show_help():
+    print("Commands:")
+    print("/bye - Exit the chat")
+    print("/help - Show this help message")
+    print("ctrl + c - Stop the model from responding")
+    print("ctrl + d - Exit the chat")
+    print()
+
+
+def generate_response(user_query: str, model_name: str) -> None:
+    model_response = "" 
+
+    for chunk in chat_streaming(user_query, model_name):
+        model_response += chunk
+        print(chunk, end="", flush=True)
+    print()
+
+    add_msg_to_memory(user_query, model_response)
+
+def handle_user_query(user_query: str, model_name: str) -> None:
+    if user_query == "/bye":
+        print("Goodbye!")
+        exit()
+    elif user_query == "/help" or user_query == "/?":
+        show_help()
+    else:
+        generate_response(user_query, model_name)
+
+
 def chat_cli(model_name: str) -> None:
     while True:
         try:
@@ -12,18 +41,11 @@ def chat_cli(model_name: str) -> None:
                 placeholder='Send a message (/? for help)',
             )
 
-            model_response = "" 
+            handle_user_query(user_query, model_name)           
 
-            for chunk in chat_streaming(user_query, model_name):
-                model_response += chunk
-                print(chunk, end="", flush=True)
-            print()
-
-            add_msg_to_memory(user_query, model_response)
-
-        except KeyboardInterrupt: 
-            # Ctrl-C interrupt
-            break
+        except KeyboardInterrupt:
+            # Ctrl-C to stop the model from responding
+            print("\nUse Ctrl + d or /bye to exit.")
         except EOFError:
             # Ctrl-D to exit
             break
