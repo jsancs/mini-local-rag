dc8811ea74d7dd5d0074aaab5013df157198a679
jsancs <javivisan19@gmail.com>
2024-10-14 19:50:18 +0200

    create embeddings

--- cli.py
+++ cli.py
@@ -1,13 +1,16 @@
 import argparse
+from typing import List
 from prompt_toolkit.shortcuts import prompt
 
 from chat import chat_streaming, add_msg_to_memory
+from embeddings import create_collection
 from utils import handle_model
 
 
 def show_help():
     print("Commands:")
     print("/bye - Exit the chat")
+    print("/add - Add document to the model's memory")
     print("/help - Show this help message")
     print("ctrl + c - Stop the model from responding")
     print("ctrl + d - Exit the chat")
@@ -24,12 +27,40 @@ def generate_response(user_query: str, model_name: str) -> None:
 
     add_msg_to_memory(user_query, model_response)
 
+def add_document() -> List[str]:
+    doc_paths = []
+    while True:
+        doc_path = prompt(
+            "Local path to document document (/done to finish): ",
+        )
+        if doc_path == "/done":
+            break
+
+        if doc_path:
+            doc_paths.append(doc_path)
+        
+    return doc_paths
+
 def handle_user_query(user_query: str, model_name: str) -> None:
     if user_query == "/bye":
         print("Goodbye!")
         exit()
     elif user_query == "/help" or user_query == "/?":
         show_help()
+    elif user_query == "/add":
+        collection_name = ""
+        documents = add_document()
+        if documents:
+            collection_name = prompt(
+                "Enter a name for the collection: ",
+            )
+
+        print(f"Adding documents to collection: {collection_name}")
+        print(f"Docs selected: {documents}")
+
+        create_collection(documents, collection_name)
+
+
     else:
         generate_response(user_query, model_name)
 
--- embeddings.py
+++ embeddings.py
@@ -1,11 +1,36 @@
 from typing import List
 
 import ollama
+import os
+import numpy as np
 
 
-def generate_embeddings(src_text: str, model_name: str) -> List[float]:
+def create_collection(
+    doc_paths: List[str],
+    collection_name: str,
+) -> None:
+    print(f"Creating collection: {collection_name}...")
+    embeddings: List[float] = []
+
+    for doc_path in doc_paths:
+        with open(doc_path, "r") as f:
+            text = f.read()
+            emb = _generate_embeddings(text)
+            embeddings.append(emb)
+
+    embeddings = np.array(embeddings)
+    
+    if not os.path.exists("collections"):
+        os.makedirs("collections")
+    np.save(f"collections/{collection_name}.npy", embeddings)
+
+
+def _generate_embeddings(
+        src_text: str,
+        model_name: str = "all-minilm",
+    ) -> List[float]:
     emb = ollama.embeddings(
         model=model_name,
         prompt=src_text,
     )
-    return emb
+    return emb["embedding"]
